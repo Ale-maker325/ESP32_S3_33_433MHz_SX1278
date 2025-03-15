50865710ae056f8058c6ab884a95f951bec3da2b
Ale-maker325 <75394336+Ale-maker325@users.noreply.github.com>
2025-03-15 22:24:30 +0200

    1

---
+++ src/main.cpp
@@ -0,0 +1,139 @@
+#include <Arduino.h>
+#include <settings.h>
+#include <rgb_led.h>
+#include <radioLR.h>
+#include <display.h>
+
+
+
+uint64_t count = 0;           //Счётчик для сохранения количества отправленных/полученных пакетов
+boolean FUN_IS_ON = false;    //Логический флаг включения/отключения вентилятора охлаждения
+
+
+
+
+
+
+
+
+void setup() {
+    #ifdef DEBUG_PRINT
+        Serial.begin(115200);
+        Serial.println(" ");
+        Serial.println(F("*************************  Serial ready 115200 *******************************"));
+        Serial.println(" ");
+    #endif
+
+    SPI_MODEM.begin(SCK_RADIO, MISO_RADIO, MOSI_RADIO);
+
+    #ifdef DEBUG_PRINT
+        Serial.println(" ");
+        Serial.print("Chip Model = ");
+        Serial.println(ESP.getChipModel());
+        Serial.print("Cpu Freq = ");
+        Serial.println(ESP.getCpuFreqMHz());
+        Serial.print("ChipRevision = ");
+        Serial.println(ESP.getChipRevision());
+        Serial.print("SDK Version = ");
+        Serial.println(ESP.getSdkVersion());
+        Serial.println(" ");
+    #endif
+
+    //инициализируем дисплей
+    displayInit();
+    
+
+    #ifdef DEBUG_PRINT
+        Serial.print(TABLE_LEFT);
+        Serial.print(F("DISPLAY INIT"));
+        Serial.println(TABLE_RIGHT);
+        Serial.println(SPACE);
+    #endif
+
+    pinMode(FUN, OUTPUT);                   //Контакт управления вентилятором охлаждения
+    
+
+
+    WriteColorPixel(COLORS_RGB_LED::red);   //Зажигаем красный цвет
+
+    setRadioMode();
+
+    radio1.setRfSwitchPins(RX_EN_PIN_1, TX_EN_PIN_1);         //Назначаем контакты для управлением вкл./выкл. усилителем передатчика
+    
+
+    //Устанавливаем функцию, которая будет вызываться при отправке пакета данных
+    radio1.setPacketSentAction(flag_operationDone_1);
+
+    radioBeginAll();
+
+    #ifdef DEBUG_PRINT
+        Serial.println(SPACE);
+    #endif
+
+    //Устанавливаем наши значения, определённые ранее в структуре config_radio1
+    radio_setSettings(radio1, config_radio1, "1");
+
+    #ifdef TRANSMITTER   //Если определена работа модуля как передатчика
+        
+
+        //Начинаем передачу пакетов
+        Serial.println(F("Sending first packet ... "));
+
+        String str = F("START!");
+        
+        transmit_and_print_data(str);
+        WriteColorPixel(COLORS_RGB_LED::green);
+
+        // и вентилятор охлаждения не включен, то включаем вентилятор охлаждения
+        if(config_radio1.outputPower > 10 && FUN_IS_ON != true)
+        {
+            //отмечаем вентилятор как включенный
+            FUN_IS_ON = true;
+            //и включаем его
+            digitalWrite(FUN, HIGH);
+        }
+
+    #endif
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void loop() {
+    delay(500);
+    WriteColorPixel(COLORS_RGB_LED::black);
+    
+    #ifdef TRANSMITTER   //Если определен как передатчик проверяем, была ли предыдущая передача успешной
+        
+        // if(operationDone_1) {
+            #ifdef DEBUG_PRINT
+                Serial.println("..................................................");
+            #endif
+            //Сбрасываем сработавший флаг прерывания
+            operationDone_1 = false;
+
+            //готовим строку для отправки
+            String str = "#" + String(count++);
+            transmit_and_print_data(str);
+            WriteColorPixel(COLORS_RGB_LED::green);
+            
+        // }
+
+    #endif
+
+   
+}
+
---
+++ src/samples/33TX_ESP32_S3/rgb_led.h
@@ -0,0 +1,65 @@
+#ifndef _____rgb_led______
+#define _____rgb_led______
+
+#include <Arduino.h>
+#include "soc/soc_caps.h"         //Для управління світодіодом
+#include "esp32-hal-rgb-led.h"    //Для управління світодіодом
+
+#define RGB_BRIGHTNESS 10       //Регулировка яркости светодиода (max 255)
+
+
+enum class COLORS_RGB_LED
+{
+    red,
+    green,
+    blue,
+    black,
+
+};
+
+
+
+void WriteColorPixel(COLORS_RGB_LED color)
+{
+    
+
+
+    switch (color)
+    {
+    case COLORS_RGB_LED::red :
+        neopixelWrite(LED_PIN,0,RGB_BRIGHTNESS,0); // Red
+        break;
+    case COLORS_RGB_LED::green:
+        neopixelWrite(LED_PIN,RGB_BRIGHTNESS,0,0); // Green  
+        break;
+    case COLORS_RGB_LED::blue:
+        neopixelWrite(LED_PIN,0,0,RGB_BRIGHTNESS); // Blue
+        break;
+    case COLORS_RGB_LED::black:
+        neopixelWrite(LED_PIN,0,0,0); // Off / black  
+        break;
+
+    default:
+        break;
+    }
+    
+}
+
+// #ifdef RGB_BUILTIN
+//   // digitalWrite(RGB_BUILTIN, HIGH);   // Turn the RGB LED white
+//   neopixelWrite(RGB_BUILTIN,RGB_BRIGHTNESS,RGB_BRIGHTNESS,RGB_BRIGHTNESS); // Red
+//   delay(1000);
+//   // digitalWrite(RGB_BUILTIN, LOW);    // Turn the RGB LED off
+//   neopixelWrite(RGB_BUILTIN,0,0,0); // Red
+//   delay(1000);
+
+//   neopixelWrite(RGB_BUILTIN,RGB_BRIGHTNESS,0,0); // Red
+//   delay(1000);
+//   neopixelWrite(RGB_BUILTIN,0,RGB_BRIGHTNESS,0); // Green
+//   delay(1000);
+//   neopixelWrite(RGB_BUILTIN,0,0,RGB_BRIGHTNESS); // Blue
+//   delay(1000);
+//   neopixelWrite(RGB_BUILTIN,0,0,0); // Off / black
+//   delay(1000);
+// #endif
+#endif;
